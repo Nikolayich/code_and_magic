3adfbadcec63377404777d2eadc451c00b357112
Nikolayich <nikolayich@hotmail.com>
2019-11-23 21:53:59 +0300

    Написал функцию рендера облака

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,16 @@
 'use strict';
 
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+}
+
 window.renderStatistics = function (ctx, names, times) {
-  
+  renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 }
