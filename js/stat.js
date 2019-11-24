fca4e5c2087377eca6ddd6ac7e7c1d421fdc4569
Nikolayich <nikolayich@hotmail.com>
2019-11-25 01:41:03 +0300

    Сделал фигурное облако вместо обычного

--- js/stat.js
+++ js/stat.js
@@ -6,8 +6,20 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 
 var renderCloud = function (ctx, x, y, color) {
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + CLOUD_WIDTH / 2, y + CLOUD_HEIGHT / 6);
+  ctx.lineTo(x + CLOUD_WIDTH, y);
+  ctx.lineTo(x + CLOUD_WIDTH - CLOUD_HEIGHT / 6, y + CLOUD_HEIGHT / 2);
+  ctx.lineTo(x + CLOUD_WIDTH, y + CLOUD_HEIGHT);
+  ctx.lineTo(x + CLOUD_WIDTH - CLOUD_WIDTH / 2, y + CLOUD_HEIGHT - CLOUD_HEIGHT / 6);
+  ctx.lineTo(x, y + CLOUD_HEIGHT);
+  ctx.lineTo(x + CLOUD_HEIGHT / 6, y + CLOUD_HEIGHT / 2);
+  ctx.lineTo(x, y);
+  ctx.closePath();
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fill();
+
 }
 
 window.renderStatistics = function (ctx, names, times) {
