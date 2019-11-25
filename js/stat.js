4230e545ac5ccaa59a9b1ec1e05a886089c5853c
Nikolayich <nikolayich@hotmail.com>
2019-11-26 00:50:34 +0300

    Добавил текст в отрисовку облака

--- js/stat.js
+++ js/stat.js
@@ -19,10 +19,14 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.closePath();
   ctx.fillStyle = color;
   ctx.fill();
-
 }
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура! Вы победили!', CLOUD_X + CLOUD_HEIGHT / 4, CLOUD_Y + CLOUD_HEIGHT / 4);
+  ctx.fillText('Список результатов:', CLOUD_X + CLOUD_HEIGHT / 4, CLOUD_Y + CLOUD_HEIGHT / 3);
 }
